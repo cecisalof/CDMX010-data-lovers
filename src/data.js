401f3aff45942c1f32b496501a124618d1b1cd79
Cecilia Losada <cecilia.sanchezlosada@gmail.com>
2021-01-28 16:39:32 -0600

    Tarjetas de deportes funcioan como botones. Funcionalidad de click a FRAME 2 funciona, aunque muestra todos los eventos.

--- src/data.js
+++ src/data.js
@@ -4,21 +4,9 @@
 
 import data from './data/athletes.js';
 
-//let arrayAthletes = data.athletes;
-
-
-/*
-for(const key in data.athletes){
-  // console.log('athletes name', data.athletes[key].name);
-   console.log('sport', data.athletes[key].sport);
- }
-  
-export default data  
-
- */
 
 //Esta función filtra del set los objetos "athlete" por su propiedad {sport}
- export function filterBySport(sport){
+export function filterBySport(sport){
     let filteredData1 = data.athletes.filter(function(athlete){
       return athlete.sport === sport
       });
@@ -28,8 +16,9 @@ export default data
 //console.log("Filtering by Sport for Volleyball", filterBySport("Volleyball"));
 
 
+//Filtrado de los 34 deportes del set de datos.
 
-   function allSportsOnce(athletes){ //athletes como parámetros
+export function allSportsOnce(athletes){ //athletes como parámetros
     let sports = []; // Un array vacío en el que se añadirán los elementos que resulten del método push.
     for(const i in data.athletes){ // un loop sobre todo el array de objetos "athletes" en "data" 
       sports.push(data.athletes[i].sport) // por el nombre del array, se aplica el método .push y dentro del paréntesis van los elementos que se
@@ -37,14 +26,48 @@ export default data
                                             // con el loop que itera sobre data.athletes.sport y añade los elemntos a "sports"
    }
    //console.log(sports);//Hasta aquí imprime 2023 objetos "athletes"
-    const dataArrSports = new Set(sports); // Se crea una variable para asiganr un nuevo set de datos únicos
-   let sportsList = [...dataArrSports]; // spread operator aplicado al nuevo array "dataArrSports" 
-   console.log(sportsList); // Ahora imprime sólo 34 elementos que responden a la propiedad {sport} en los objetos "athletes" de "data"
+    const sportsArr = new Set(sports); // Se crea una variable para asiganr un nuevo set de datos únicos
+   let sportsList = [...sportsArr]; // spread operator aplicado al nuevo array "dataArrSports" 
+   //let sportArrInOrder = athletes.sort(); // ¿C+omo ordenar alfabéticamente?
+   //console.log(sportsList); // Ahora imprime sólo 34 elementos que responden a la propiedad {sport} en los objetos "athletes" de "data"
    return sportsList;
-}
+  }
+  
   allSportsOnce(data); //Si no pasamos "data" como argumento de la función, ésta no correrá
 
 
 
   
+export function filterByEvent(athletes){ 
+     let eventResult = [];
+     for (const i in data.athletes){
+     eventResult.push(data.athletes[i].event)
+     } 
+     //console.log(eventResult); // Hasta aqu{i arroja los eventos deportivos repetdos
+    const eventArr = new Set(eventResult);
+    let eventList = [...eventArr];
+    //console.log(eventList); // Ahora arroja un set único de eventso deportivos (306)
+    
+    return eventList;
+   }
+   
+   filterByEvent(data);
+
+   
+  
+   //OH CON RAÚL SOBRE MAPEAR LOS EVENTOS DE COMPETICIÓN POR DEPORTE
+   export function getEventsBySport(sport) {
+     const filteredAthletes = data.athletes.filter(function(athlete){ //filtrado por deporte
+      return athlete.sport === sport
+     });
+      const events = filteredAthletes.map(function(athlete){ // crear un nuevo array con los resultados de filtrar los eventos de cada deporte 
+    
+       return athlete.event
+     })
+    
+     const eventsWithoutDuplicate = new Set(events) 
+     return [...eventsWithoutDuplicate]
+   }
+     getEventsBySport(data);
+    //console.log("Filtering by Sport for Swimmig", getEventsBySport("Swimming"));
   
--- src/main.js
+++ src/main.js
@@ -3,6 +3,98 @@
 //Operaciones como creación de nodos, registro de manejadores de eventos (event listeners o event handlers), ....
 
 import data from './data/athletes.js';
-import {} from './data.js';
-//, sports, filterByEvent, getEventsBySport } from './data.js';
+import {allSportsOnce, filterByEvent, filterBySport, getEventsBySport} from './data.js';
+
+let arrayAthletes = data.athletes;
+let eventList = filterByEvent(data);
+
+
+// 1.- Crear tarjetas de deportes con template literals.
+function createSportCards(sport){
+    
+    let cardItem = `<li id="buttonCard">
+    <button id="SportCard" class="SportCard">
+        <img id="sportIcon" src="./resources/icons_cutted/${sport}.png">
+    </button>
+    </li>
+    `;
+    
+    return cardItem;
+}
+
+//2.- Imprimir tarjetas en el navegador 
+function setSportCard(){
+    let card = document.getElementById("sportsCards"); //asignamos el elmento con ID a la variable "card"
+    let arrSports = allSportsOnce(arrayAthletes); // asignamos el set de datos a la varibale "arrSport"
+    let html = ""; // declaramos una variable vacía, que guardará el valor de la función createSportCards
+    arrSports.forEach(sport => html += createSportCards(sport)) // igualamos la variabel html a la función createSportCards y le pasamos el parámetro sport. 
+    card.innerHTML = html; // Imprimimos el resultado de la función createSportCards en el navegador. El resultados son las tarjetas de {iconos}
+
+}
+
+// 3.- Crear lista de eventos por deporte
+function createEventList(event,sport) {
+let listItem =  `<li id="eventList">
+     <img id="sportIcon" src="./resources/icons_cutted/${sport}.png">
+     <p id="event">${event}</p>
+     </li>`;
+    
+     return listItem;
+ }
+   
+ // 4.- Imprimir lista de eventos por deporte 
+ function setEventList(){
+     let containerList = document.getElementById("eventList");
+     let html2 = '';
+     eventList.forEach(event => html2 += createEventList(event));
+     containerList.innerHTML = html2;
+ }
+//console.log(SeteventList);
+
+
+
+ // 5.- Función que, al cargar el navegador, oculta el FRAME 1 y muestra el FRAME 2
+window.addEventListener('DOMContentLoaded', () => {
+    setSportCard();
+    let buttons = document.getElementsByClassName('SportCard');
+    let sportsCard = document.getElementById('sportsCards');
+    let eventList = document.getElementById('eventList');
+   
+    for(let i = 0; i < buttons.length ; i++) {
+        buttons[i].addEventListener('click', () => {
+            sportsCard.style.display = "none"; // ocultamos tarjetas de deportes
+            eventList.style.display = "block";
+        });
+    }
+});
+
+
+/* 6.- Función que, al cargar el navegador, filtra los eventos por deporte
+window.addEventListener('DOMContentLoaded', () => {
+    setEventList();
+    let list = document.getElementsByClassName('eventList');
+    let eventList = document.getElementById('eventList');
+    //console.log(eventList);
+    for(let i = 0; i < list.length ; i++) {
+        if (sport.leght =  )    
+        list[i].addEventListener('click', () => {
+
+        
+            getEventsBySport.style.display= "block"
+            eventList.style.display = "none";
+        });
+    }
+    
+});
+
+    
+*/
+
+// Esta llamada muestra todos eventos deportivos sin filtrarlos por deporte. 
+window.addEventListener('DOMContentLoaded', setEventList());
+   
+
+
+
+
 
